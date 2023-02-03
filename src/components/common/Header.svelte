19cf87e45af0387f283bdb4698c6fee4a52daa19
rinjugatla <rin.jugatla@gmail.com>
2023-02-03 23:41:07 +0900

    基本コンポーネントを追加

---
+++ src/components/common/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { assets } from "$app/paths";
+    import { page } from "$app/stores";
+
+    const MENUS = [
+        {href: "/", title: "Root"},
+        {href: "/Home", title: "Home"},
+        {href: "/about/", title: "About"},
+        {href: "/diary/", title: "Diary"},
+        {href: "/create/", title: "Create"},
+    ]
+</script>
+
+<header>
+    <h1>ヘッダ</h1>
+    <nav class="menu">
+        <ul>
+        {#each MENUS as menu}
+             <li>
+                {#if $page.url.pathname === menu.href}
+                    <strong>{menu.title}</strong>
+                {:else}
+                    <a href={menu.href}>{menu.title}</a>
+                {/if}
+             </li>
+        {/each}
+        </ul>
+    </nav>    
+</header>
+
+<style>
+    .menu {
+
+    }
+    li {
+        display: inline-block;
+        list-style: none;
+        background-color: lightpink;
+        margin-left: 20px;
+    }
+</style>
