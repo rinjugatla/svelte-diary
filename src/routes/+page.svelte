ad11661d16a59b1d50d4d64871046376323af7c5
rinjugatla <rin.jugatla@gmail.com>
2023-02-03 23:53:30 +0900

    各ページのひな型を作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
 	import Header from './../components/common/Header.svelte';
 	import Footer from './../components/common/Footer.svelte';
-
-    export let url = "";
 </script>
 
 <main>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Header from './../../components/common/Header.svelte';
+	import Footer from './../../components/common/Footer.svelte';
+</script>
+
+<main>
+    <Header/>
+    <h1>Hello</h1>
+    <Footer/>
+</main>
