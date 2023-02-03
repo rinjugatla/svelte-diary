ef2f745ee54f5bc95d16eaa7526b91853ac0ab7a
rinjugatla <rin.jugatla@gmail.com>
2023-02-04 00:01:13 +0900

    レイアウトを適用

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '$lib/common/Header.svelte';
+	import Footer from '$lib/common/Footer.svelte';
+</script>
+
+<Header />
+
+<main>
+    <slot />
+</main>
+
+<Footer />
--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-	import Header from './../../components/common/Header.svelte';
-	import Footer from './../../components/common/Footer.svelte';
+
 </script>
 
 <main>
-    <Header/>
-    <h1>Hello</h1>
-    <Footer/>
+    <h1>create</h1>
 </main>
