d591627cca6a1c468376c6e5570860de332161b0
rinjugatla <rin.jugatla@gmail.com>
2023-02-04 00:27:48 +0900

    Tailwind CSS、Flowbite-Svelte追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
-    import Header from '$lib/common/Header.svelte';
-	import Footer from '$lib/common/Footer.svelte';
+  import "../app.postcss";
+  import Header from "$lib/common/Header.svelte";
+  import Footer from "$lib/common/Footer.svelte";
 </script>
 
 <Header />
 
 <main>
-    <slot />
+  <slot />
 </main>
 
 <Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-
+    import { Alert } from 'flowbite-svelte';
 </script>
 
 <main>
     <h1>root</h1>
+
+    <div class="p-8">
+        <Alert>
+            <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+        </Alert>
+    </div>
 </main>
