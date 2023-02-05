a1bcfbda556d7248aa6e31e4ae519b81eb6a974a
rinjugatla <rin.jugatla@gmail.com>
2023-02-05 22:23:40 +0900

    日記の一覧を表示、レートを星マークで表示する機能を追加

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,12 +2,13 @@
 	import { fetchDiaries } from './../../lib/helper/api.js';
     import { onMount, onDestroy } from 'svelte';
 	import { UserId } from './../../lib/store.js';
+    import StarRating from 'svelte-star-rating';
 
     let author_id = null;
 
     const unsubscribe = UserId.subscribe( id => author_id = id);
 
-    let promise;
+    let promise = fetchDiaries();
     onMount ( async () => {
         promise = await fetchDiaries(author_id);
         console.log(promise);
@@ -18,6 +19,17 @@
     });
 </script>
 
-<main>
-    <h1>home</h1>
-</main>
+{#await promise}
+    <p>Loading</p>
+{:then diaries}
+    {#each diaries as diary}
+         <a href="/diary/{diary.id}" class="flex items-center">
+            <aside>
+                <p>{diary.created_at}</p>
+                image
+                <p><StarRating rating={diary.rate / 2 }/></p>
+            </aside>
+            <p>{diary.body}</p>
+         </a>
+    {/each}
+{/await}
